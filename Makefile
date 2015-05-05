d39a11e28e855a64c4bd121415cd363ef8105a6e
John Foley <jfoley@cs.umass.edu>
2015-05-05 18:31:09 -0400

    Add sydney specifics to gen.py, remove makefile since we should generate it

--- Makefile
+++
@@ -1,8 +0,0 @@
-JAVA:='/home/jfoley/bin/java8/bin/java'
-JAR:='extract-dataset/target/extract-dataset-1.0-SNAPSHOT.jar'
-
-%.galago: %.trectext.gz
-	${JAVA} -cp ${JAR} org.lemurproject.galago.core.tools.App build --inputPath=$< --indexPath=$@
-
-.PHONY: indices
-indices: cat.c10.galago cat.c5.galago cat.n10.galago cat.n5.galago cat.p10.galago cat.p5.galago cat.title-only.galago
---
+++ make.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python2
+
+import socket
+
+host = socket.gethostname()
+
+boilerplate = """
+JAVA:=$(shell echo $(JAVA_HOME)/bin/java)
+JAR:='extract-dataset/target/extract-dataset-1.0-SNAPSHOT.jar'
+"""
+print(boilerplate)
+
+if host == 'sydney.cs.umass.edu':
+    print("PREFIX:='qsub -b y -cwd -sync y '")
+else:
+    print("PREFIX:=''")
+
+# default rule
+print(".PHONY: default")
+print(".DEFAULT: default")
+print("default:")
+print("\t@echo ${PREFIX} ${JAVA}")
+print
+
+
+indexingRule = """
+%.galago: %.trectext.gz
+	${PREFIX} ${JAVA} -cp ${JAR} org.lemurproject.galago.core.tools.App build --inputPath=$< --indexPath=$@
+"""
+print(indexingRule)
+
+indexes = []
+for method in ["n100", "n10", "n5", "p10", "p5", "c10", "c5", "title-only"]:
+    indexes += ["cat.%s.galago" % method]
+    print("cat.%s.trectext.gz:" % method)
+    print("\t${PREFIX} ${JAVA} -cp ${JAR} wikicat.extract.catgraph.BuildCategoryIndex --method=%s --output=$@" % method)
+    print
+
+print(".PHONY: indexes")
+print("indexes: " + ' '.join(indexes))
+
