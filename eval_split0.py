06d6089ab291c704eabf6fab2852c24e269adc8a
John Foley <jfoley@cs.umass.edu>
2015-05-05 23:51:41 -0400

    Skip comparisons because pairwise randomization test with 10k queries takes an incredible amount of time :(

--- eval_split0.py
+++ eval_split0.py
@@ -3,5 +3,5 @@ import sys
 runs = ' '.join(["--runs+"+x for x in sys.argv[1:]])
 
 print("""
-${JAVA_HOME}/bin/java -cp extract-dataset/target/extract-dataset-1.0-SNAPSHOT.jar org.lemurproject.galago.core.tools.App eval --judgments=split0.qrel %s
+${JAVA_HOME}/bin/java -cp extract-dataset/target/extract-dataset-1.0-SNAPSHOT.jar org.lemurproject.galago.core.tools.App eval --comparisons=false --metrics+map --metrics+ndcg --metrics+p1 --metrics+p10 --judgments=split0.qrel %s
 """ % (runs))
